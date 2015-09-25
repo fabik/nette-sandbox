4392854303f8a6c7189e8bcdc99993cd12adda9e
Adam Stipak <adam.stipak@gmail.com>
2015-09-25 21:18:45 +0200

    Init.

---
+++ .docker/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build --rm=true -t nette/sandbox .
---
+++ .docker/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker run -i -t --rm -v "$PWD:/data" -w /data \
+  -p 80:80 \
+  nette/sandbox:latest \
+  sh -c 'service mysql start;\
+    mysqladmin -u root -proot create test;\
+    service apache2 start;\
+    cd /data && composer install;\
+    bash'
